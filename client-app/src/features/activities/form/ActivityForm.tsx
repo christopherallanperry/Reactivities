bd931c1d581393c624e9fa95bea6ef4aae833855
chrisofarabia <christopherallanperry@yahoo.co.uk>
2019-10-04 23:39:42 +0100

    Added functionality to allow creation of new activities and edit of exisiting - issues to be resolved.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,16 @@
 import React, { useState, FormEvent } from "react";
 import { Segment, Form, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
+import { v4 as uuid } from "uuid";
 
 interface IProps {
     setEditMode: (editMode: boolean) => void;
     activity: IActivity;
+    createActivity: (activity: IActivity) => void;
+    editActivity: (activity: IActivity) => void;
 }
 
-const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState }) => {
+const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState, createActivity, editActivity }) => {
     const initializeForm = () => {
         if (initialFormState) {
             return initialFormState;
@@ -27,7 +30,15 @@ const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormStat
     const [activity, setActivity] = useState<IActivity>(initializeForm);
 
     const handleSubmit = () => {
-        console.log(activity);
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid(),
+            };
+            createActivity(newActivity);
+        } else {
+            editActivity(activity);
+        }
     };
 
     const handleInputChange = (event: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
