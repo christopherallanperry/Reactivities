e3b0182b6b96cb152f3796381b69243134da6b70
chrisofarabia <christopherallanperry@yahoo.co.uk>
2019-10-07 22:06:59 +0100

    Added loading indicator to activities list.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,12 +4,14 @@ import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponent";
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([]);
     const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
 
     const [editMode, setEditMode] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const handleSelectActivity = (id: string) => {
         setSelectedActivity(activities.filter(a => a.id === id)[0]);
@@ -44,15 +46,19 @@ const App = () => {
     };
 
     useEffect(() => {
-        agent.Activities.list().then(response => {
-            let activities: IActivity[] = [];
-            response.forEach(activity => {
-                activity.date = activity.date.split(".")[0];
-                activities.push(activity);
-            });
-            setActivities(activities);
-        });
+        agent.Activities.list()
+            .then(response => {
+                let activities: IActivity[] = [];
+                response.forEach(activity => {
+                    activity.date = activity.date.split(".")[0];
+                    activities.push(activity);
+                });
+                setActivities(activities);
+            })
+            .then(() => setLoading(false));
     }, []);
+
+    if (loading) return <LoadingComponent content="Loading activities..." />;
 
     return (
         <Fragment>
