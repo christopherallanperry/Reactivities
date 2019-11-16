9bd0b0f259639747a6e51f0d5c68e8fd45f53b1b
chrisofarabia <christopherallanperry@yahoo.co.uk>
2019-11-16 23:39:15 +0000

    Added Toast messages for errors & handled server & network errors

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import "react-toastify/dist/ReactToastify.min.css";
 import "./app/layout/styles.css";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
@@ -10,12 +11,12 @@ import ScrollToTop from "./app/layout/ScrollToTop";
 export const history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
-  </Router>,
-  document.getElementById("root")
+    <Router history={history}>
+        <ScrollToTop>
+            <App />
+        </ScrollToTop>
+    </Router>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
